8c04e6dc2b901153f3b38f4a7c78083e6fc4cb99
Kevin <kevelberger@gmail.com>
2018-05-07 07:22:03 -0700

    Join forms, read in template mjml file

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 var express = require("express");
 var app     = express();
+var fs      = require('fs');
 var path    = require("path");
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 var execSync = require('child_process').execSync;
+
+// Generate HTML
 var cmd = path.join(__dirname + "/node_modules/.bin/mjml index.mjml -o testemail.html");
 
 var options = {
   encoding: 'utf8'
 };
 
-console.log(execSync(cmd, options));
+//execSync(cmd, options);
 
 
-app.get('/',function(req,res){
+app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
 });
+
+// Get email template and load template MJML file
+app.post('/template', (req, res) => {
+  let file;
+  const template = req.template || 'basic';
+
+  if (template === 'basic') {
+    file = getTemplate('index');
+
+    
+  }
+});
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+// Returns the template MJML file
+function getTemplate(name) {
+  return fs.readFileSync(`${name}.mjml`, 'utf-8');
+}
