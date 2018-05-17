c409e1ff9079690a5e11cfbb198747c88b740552
Kevin <kevelberger@gmail.com>
2018-05-17 07:47:08 -0700

    Toggle row highlighting and tooltip display

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const structContainer = document.querySelector('#structure-container');
 const emailBuilder = new EmailBuilder();
 const toolbar = new Toolbar();
 
+const toolbarContainer = document.querySelector('.tool-container');
+
 // Hook Dragula events into EmailBuilder methods
 const dragger = dragula([structContainer, emailContainer, blockContainer], {
   copy: emailBuilder.copy,
@@ -15,13 +17,61 @@ const dragger = dragula([structContainer, emailContainer, blockContainer], {
 .on('drop', (el, target, src, sibling) => {
   emailBuilder.handleDrop(el, target, src, sibling);  
 
-  const row = [].slice.call(document.querySelectorAll('.empty-structure-block')).pop();
+  const row = el.classList.contains('structure');
 
   if (row) {
-    console.log('row is', row);
-    
-    $(row).mouseenter(() => {
-      toolbar.show(row);
-    });
+    toggleRowHighlight(el);
   }
 });
+
+
+$(toolbarContainer).mouseenter(() => {
+  toolbarContainer.classList.add('show');
+});
+
+$(toolbarContainer).mouseleave(() => {
+  toolbarContainer.classList.remove('show');
+});
+
+setInterval(() => {
+  const noRowHovered = $('.structure.hovered').length === 0;
+  const onToolbar = toolbarContainer.classList.contains('show');
+
+  if (noRowHovered && ! onToolbar) {
+    toolbar.hide();
+  }
+}, 1000);
+
+function toggleRowHighlight(el) {
+  let hovering = false;
+
+  $(el).mouseenter(() => {
+    hovering = true;
+    el.classList.add('hovered');
+    toolbar.show(el);
+    $('.structure').filter((idx, elm) => {
+      if (elm !== el) elm.classList.remove('hovered');
+    })
+  });
+
+  $(el).mouseleave(() => {
+    setTimeout(() => {
+      const onToolbar = toolbarContainer.classList.contains('show');
+
+      if (! onToolbar) {
+        el.classList.remove('hovered');
+      }
+
+      const rowIsHovered = $('.structure.hovered').length > 0;
+
+      if (! rowIsHovered && ! onToolbar) {
+        hovering = false;
+      }
+
+      if (! onToolbar && ! hovering) {
+        el.classList.remove('hovered');
+        toolbar.hide();
+      }
+    }, 1000);
+  });
+}
