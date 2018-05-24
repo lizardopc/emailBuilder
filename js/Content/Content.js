2d752da2e55424edca0085c4aacdaffcb8d4c67b
kevinelberger <kevelberger@gmail.com>
2018-05-24 16:29:38 -0700

    Add image content and base content methods

--- js/Content/Content.js
+++ js/Content/Content.js
@@ -2,5 +2,20 @@ export default class Content {
   constructor(config) {
     this.fields = [];
     this.type = config.type;
+    this.form = $('.content-settings-body');
+  }
+
+  addFieldsToForm() {
+    this.fields.forEach(field => {
+      this.form.appendChild(field);
+    });
+  }
+
+  validate() {
+
+  }
+
+  save() {
+
   }
 }
---
+++ js/Content/Image.js
@@ -0,0 +1,5 @@
+export default class Image extends Content {
+  constructor() {
+
+  }
+}
